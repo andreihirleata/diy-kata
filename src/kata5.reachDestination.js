4cbc63216b3b4ddf2aa033e5ffd4d32d8cfafd37
andreihirleata <hirleata.andrei@gmail.com>
2020-03-15 12:39:42 +0000

    solved reachDestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,11 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  if (typeof distance === `number` && typeof speed === `number`) {
+    // Math.ceil(num / increment) * increment; - rounds to the nearest increment
+    const eta = distance / speed;
+    const roundedEta = Math.ceil(eta / 0.5) * 0.5;
+    return `I should be there in ${roundedEta} hours`;
+  }
+  return "Input is not a number";
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,10 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {
+    expect(reachDestination(100, 10)).toEqual("I should be there in 10 hours");
+    expect(reachDestination("stringTest", 1)).toEqual("Input is not a number");
+    expect(reachDestination(44, 10)).toEqual("I should be there in 4.5 hours");
+    expect(reachDestination(22.2, 3.2)).toEqual("I should be there in 7 hours");
+  });
 });
