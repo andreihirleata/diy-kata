f0830554b7cbbb12abf7d8f14101fbd03b52b998
andreihirleata <hirleata.andrei@gmail.com>
2020-03-15 13:51:56 +0000

    solved joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,15 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+  let nameString;
+  const nameArray = namesObj.map(x => x.name);
+  if (nameArray.length === 0)
+    return "No contained within objects within the array";
+  if (nameArray.length === 1) {
+    nameString = nameArray.join();
+    return `${nameString}`;
+  }
+  const lastItem = nameArray.pop();
+  nameString = nameArray.join(", ");
+  return `${nameString} & ${lastItem}`;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,23 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([
+        {
+          name: "dan",
+          age: 10
+        },
+        { name: "anon", hobby: "test" },
+        { name: "carrie" }
+      ])
+    ).toEqual("dan, anon & carrie");
+    expect(
+      joinNames([
+        {
+          name: "Andrei"
+        }
+      ])
+    ).toEqual("Andrei");
+  });
 });
